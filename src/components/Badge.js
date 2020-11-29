5e4a19ef19119fd488486e932e2b06e94a30d6fd
juliodev <juliocesar8881@hotmail.com>
2020-11-29 16:45:28 -0500

    Aprendiendo la utilidad de los props

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -3,19 +3,29 @@ import Logo from '../Images/badge-header.svg'
 import "./Styles/Badge.css"
 
 class Badge extends React.Component{
-    render(){ //El metodo render define el resultado que vamos a ver en pantall
+    render(){ //El metodo render define el resultado que vamos a ver en pantalla
+              //Los props me permiten leer las varibles que tengo creadas en el index
+           //desestructurando las variablesen this. props y con ello solo escribo esa palabra reserva
+           //una sola vez
+              const {
+               firstName,
+               lastName,
+               avatar,
+               jobTitle,
+               twiter
+           } = this.props  
         return(
-            <div className="Badge">
+                <div className="Badge">
                 <div className="Badge__header">
                     <img src={Logo} alt="Logo de la conferencia"/>
                 </div>
                 <div className="Badge__section-name">
-                    <img className="Badge__avatar" src="https://i.imgur.com/hy10FUK.jpg" alt="Avatar"/>
-                    <h1>Julio César <br/>Ardila</h1>
+                    <img className="Badge__avatar" src={avatar} alt="Avatar"/>
+                    <h1>{firstName} <br/>{lastName}</h1> 
                 </div>
                 <div className="Badge__section-info">
-                    <h3>Frontend ingineer</h3>
-                    <div>@juliodevs</div>
+                  <h3>{jobTitle}</h3>
+                      <div>@{twiter}</div>
                 </div>
                 <div className="Badge__footer">
                     #Platziconf
--- src/index.js
+++ src/index.js
@@ -20,4 +20,11 @@ import 'bootstrap/dist/css/bootstrap.css'
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(<Badge/>, container);
+ReactDOM.render(
+<Badge 
+firstName='Julio'
+lastName='Ardila'
+jobTitle='Fronted ingeneer'
+twiter='juliodevs'
+avatar='https://i.imgur.com/hy10FUK.jpg'
+/>, container);
