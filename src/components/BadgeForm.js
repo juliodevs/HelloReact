8e134fca06d8e2a821c02c1d3190a12c788f183f
juliodev <juliocesar8881@hotmail.com>
2020-12-08 20:51:07 -0500

    Adicionando state y setState al proyecto

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -2,11 +2,24 @@ import React from 'react'
 
 class BadgeForm extends React.Component{
 
-    //Esta función permite mostrar en consolo lo que estoy recibiendo en el imput
+    state={
+        firstName:'',
+        lastName:'',
+        email:'',
+        jobTitle:'',
+        twiter:''
+    }//Estoy inicializando el estado. Esto siempre debo relizarlo para poder ejecutarlo mas adelante
+// La asignación de valores para los objetos es on el igual y para lo elementos dentro del objeto es dos puntos
+    //Esta función permite mostrar en consola lo que estoy recibiendo en el imput
     handlechange = e =>{
-     console.log({
-            name:e.target.name, //Pemite imprimir en consola el name  del label
-            value:e.target.value}) // Permite ver en consolo el value del label
+    //  console.log({
+    //         name:e.target.name, //Pemite imprimir en consola el name  del label
+    //         value:e.target.value}) // Permite ver en consolo el value del label
+
+    this.setState({
+        [e.target.name]: e.target.value, //Guarda la información del imput que se este ejecutando,
+        //Eejemplo: Si el imput es el email la información se guardará en la llave email
+    })
     }    
     handleClick = e =>{ //Esta función imprimi el evento que se ejecuta al dar click sobre el boton
         console.log('Click sobre el boton')
@@ -14,6 +27,7 @@ class BadgeForm extends React.Component{
     handleSubmit = e =>{
         e.preventDefault() //Con este metodo diciendo que no quiero enviar el formulario
         console.log("Se detiene el envio del formulario")
+        console.log(this.state)
     }
     render(){
         return(
@@ -22,7 +36,45 @@ class BadgeForm extends React.Component{
                 <form onSubmit={this.handleSubmit}>{/* Le paso al form función para que no se envíe el formulario */}
                     <div className="form-group"> {/* Clase de bootstrap que permite darle estilos a una etiqueta y a una caja de texto */}
                         <label >First Name</label> {/* Creando la primera etiqueta */}
-                        <input onChange={this.handlechange} className="form-control" type="text" name="firstName"/>
+                        <input
+                         onChange={this.handlechange}
+                          className="form-control" 
+                          type="text" name="firstName" 
+                          placeholder="Introduce tu nombre"
+                          value={this.state.firstName}/>
+                    </div>
+                    <div className="form-group"> 
+                        <label >Last Name</label> 
+                        <input onChange={this.handlechange}
+                         className="form-control" 
+                         type="text" name="lastName" 
+                         placeholder="Introduce tus apellidos"
+                         value={this.state.lastName}/>
+                    </div>
+                    <div className="form-group"> 
+                        <label >Email</label> 
+                        <input onChange={this.handlechange} 
+                        className="form-control"
+                         type="email" 
+                         name="email"
+                         placeholder="Introduce tu correo electronico" 
+                         value={this.state.email}/>
+                    </div>
+                    <div className="form-group"> 
+                        <label >Job Title</label> 
+                        <input onChange={this.handlechange}
+                         className="form-control"
+                          type="text" name="jobTitle"
+                          placeholder="Introdue el nombre de tu profesión"
+                           value={this.state.jobTitle}/>
+                    </div>
+                    <div className="form-group"> 
+                        <label >Twiter</label> 
+                        <input onChange={this.handlechange} 
+                        className="form-control" 
+                        type="text" name="twiter"
+                        placeholder="Introduce tu Twiter"
+                         value={this.state.twiter}/>
                     </div>
                     <button  onClick={this.handleClick} className="btn btn-primary">SAVE</button>
                 </form>
