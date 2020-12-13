f5d14efdeac0ef49479677e7be56b1918cc8c321
juliodev <juliocesar8881@hotmail.com>
2020-12-13 11:55:46 -0500

    Aprendiendo la técnica de levantamiento de estados pasando props a los diferentes componentes

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -2,32 +2,29 @@ import React from 'react'
 
 class BadgeForm extends React.Component{
 
-    state={
-        firstName:'',
-        lastName:'',
-        email:'',
-        jobTitle:'',
-        twiter:''
-    }//Estoy inicializando el estado. Esto siempre debo relizarlo para poder ejecutarlo mas adelante
+    // state={
+       
+    // }//Estoy inicializando el estado. Esto siempre debo relizarlo para poder ejecutarlo mas adelante
 // La asignación de valores para los objetos es on el igual y para lo elementos dentro del objeto es dos puntos
     //Esta función permite mostrar en consola lo que estoy recibiendo en el imput
-    handlechange = e =>{
-    //  console.log({
-    //         name:e.target.name, //Pemite imprimir en consola el name  del label
-    //         value:e.target.value}) // Permite ver en consolo el value del label
+    // handlechange = e =>{
+    // //  console.log({
+    // //         name:e.target.name, //Pemite imprimir en consola el name  del label
+    // //         value:e.target.value}) // Permite ver en consolo el value del label
 
-    this.setState({
-        [e.target.name]: e.target.value, //Guarda la información del imput que se este ejecutando,
-        //Eejemplo: Si el imput es el email la información se guardará en la llave email
-    })
-    }    
+    // this.setState({ //Permite escribir el estado del imput
+    //     [e.target.name]: e.target.value, //Guarda la información del imput que se este ejecutando,
+    //     //Eejemplo: Si el imput es el email la información se guardará en la llave email
+
+    // })
+    // }    
     handleClick = e =>{ //Esta función imprimi el evento que se ejecuta al dar click sobre el boton
         console.log('Click sobre el boton')
     }
     handleSubmit = e =>{
         e.preventDefault() //Con este metodo diciendo que no quiero enviar el formulario
         console.log("Se detiene el envio del formulario")
-        console.log(this.state)
+        console.log(this.state) //Imprimo todos los valores del estado al momento de dar click en el boton save
     }
     render(){
         return(
@@ -37,44 +34,39 @@ class BadgeForm extends React.Component{
                     <div className="form-group"> {/* Clase de bootstrap que permite darle estilos a una etiqueta y a una caja de texto */}
                         <label >First Name</label> {/* Creando la primera etiqueta */}
                         <input
-                         onChange={this.handlechange}
+                         onChange={this.props.onChange}
                           className="form-control" 
                           type="text" name="firstName" 
-                          placeholder="Introduce tu nombre"
-                          value={this.state.firstName}/>
+                           value={this.props.formValues.firstName}/> {/*Me permite leer el estado */}
                     </div>
                     <div className="form-group"> 
                         <label >Last Name</label> 
-                        <input onChange={this.handlechange}
+                        <input onChange={this.props.onChange}
                          className="form-control" 
                          type="text" name="lastName" 
-                         placeholder="Introduce tus apellidos"
-                         value={this.state.lastName}/>
+                         value={this.props.formValues.className}/>
                     </div>
                     <div className="form-group"> 
                         <label >Email</label> 
-                        <input onChange={this.handlechange} 
+                        <input onChange={this.props.onChange} 
                         className="form-control"
                          type="email" 
                          name="email"
-                         placeholder="Introduce tu correo electronico" 
-                         value={this.state.email}/>
+                         value={this.props.formValues.email}/>
                     </div>
                     <div className="form-group"> 
                         <label >Job Title</label> 
-                        <input onChange={this.handlechange}
+                        <input onChange={this.props.onChange}
                          className="form-control"
                           type="text" name="jobTitle"
-                          placeholder="Introdue el nombre de tu profesión"
-                           value={this.state.jobTitle}/>
+                          value={this.props.formValues.jopTitle}/>
                     </div>
                     <div className="form-group"> 
                         <label >Twiter</label> 
-                        <input onChange={this.handlechange} 
+                        <input onChange={this.props.onChange} 
                         className="form-control" 
                         type="text" name="twiter"
-                        placeholder="Introduce tu Twiter"
-                         value={this.state.twiter}/>
+                        value={this.props.formValues.twiter}/>
                     </div>
                     <button  onClick={this.handleClick} className="btn btn-primary">SAVE</button>
                 </form>
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -7,6 +7,26 @@ import BadgeForm from '../components/BadgeForm'
 import '../pages/styles/BadgeNew.css'
 
 class BadgeNew extends React.Component{
+    //Requiero que el estado cambie en BadgeNew y no en BadgeForm, y para hacerlo incializo en estado en BadgeNew
+        
+    state = {form:{ //Inicializo los valores del form vacios para evitar error
+        firstName:'',
+        lastName:'',
+        email:'',
+        jobTitle:'',
+        twiter:''
+    }} //Al estado vacio se le adiciona una propiedad llamada form que tambie tiene un objeto vacio interno
+   
+   handleChange = e => { // Creo metodo el cual recibe el evento
+        
+    this.setState({ // Cuando se reciba el evento va ha realizar un setState al cual le pasamos un objeto
+        form:{ //El form va a contener la información del evento que acaba de ocurrir
+            ...this.state.form, //Dejo caer todos los valores que estan en this.state.form y le añadimos un nuevo valor
+            [e.target.name]:e.target.value
+        }
+
+       })
+   }
     render(){
         return(
             <div>
@@ -18,15 +38,18 @@ class BadgeNew extends React.Component{
                    <div className="row">
                         <div className="col-6">
                             <Badge 
-                            firstName="Julio César" 
-                            lastName="Ardila" 
-                            twiter="juliocesar" 
-                            jobTitle="Fronten Engineer" 
+                            firstName={this.state.form.firstName} 
+                            lastName={this.state.form.lastName} 
+                            twiter={this.state.form.twiter} 
+                            jobTitle={this.state.form.jobTitle} 
+                            email={this.state.form.email}
                             avatar="https://i.imgur.com/hy10FUK.jpg"
                             />
                         </div>
                         <div className="col-6">
-                            <BadgeForm/>
+                            <BadgeForm
+                             onChange={this.handleChange} 
+                             formValues = {this.state.form}/> {/* Le estoy pasando el metod handleChange y los valores del formulario como props a BadgeForm */}
                         </div>
                    </div>
                </div>
