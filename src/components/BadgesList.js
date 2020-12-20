fd0e6435283b17b4e91f1eef5e025748a4263420
juliodev <juliocesar8881@hotmail.com>
2020-12-20 15:27:31 -0500

    Aplicando estilos a listas de componentes

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,16 +1,39 @@
 import React from 'react'
+import './Styles/estilosJulio.css';
+
+
+class BadgesListItems extends React.Component {
+    render() {
+        return (
+            <div className='BadgesListItem'>
+                <img className='BadgesListItem__avatar'
+                src={this.props.badge.avatarUrl} 
+                alt={`${this.props.badge.firstName} ${this.props.badge.lastName}`}
+                />
+
+                <div>
+                    <strong>
+                        {this.props.badge.firstName} {this.props.badge.lastName}
+                    </strong>
+                        <br/>@{this.props.badge.twitter}
+                        <br/>
+                        {this.props.badge.jobTitle}
+                </div>
+            </div>
+        )
+    }
+}
+
 
 class BadgesList extends React.Component{
     render(){
         return(
-        <div>
+        <div className="BadgesList">
             <ul className='list-unstyled'>
                 {this.props.Badges.map((badge)=>{
                     return(
                         <li key={badge.id}>
-                            <p>
-                                {badge.firstName} {badge.lastName}
-                            </p>
+                            <BadgesListItems badge={badge}/>
                         </li>
                     )
                 })}
